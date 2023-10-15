32f83d61f49fc97784ca08dd11ebe0f323735e40
auscode <hm6091999@gmail.com>
2023-10-15 14:47:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  const variants = [
+    {
+      id: "one",
+      textColor: "#000",
+      bgColor: "#fff",
+      productImgPath: "/assets/nike1",
+      logoImgPath: "/assets/logo2.png",
+      buttonColor: "#fff",
+    },
+    {
+      id: "two",
+      textColor: "#000",
+      bgColor: "coral",
+      productImgPath: "/assets/nike2",
+      logoImgPath: "/assets/logo2.png",
+      buttonColor: "#fff",
+    },
+    {
+      id: "three",
+      textColor: "#fff",
+      bgColor: "#000",
+      productImgPath: "/assets/nike3",
+      logoImgPath: "/assets/logo1.png",
+      buttonColor: "#000",
+    },
+  ];
+  variants.forEach((variant) => {
+    $(`.${variant.id}`).on("click", function () {
+      $("body").css({ color: variant.textColor, background: variant.bgColor });
+      $(".product-img").attr("src", `${variant.productImgPath}/1.png`);
+      $(".logo-img img").attr("src", variant.logoImgPath);
+      $(".btn").css({ color: variant.buttonColor, background: "#000" });
+      $(".variant").removeClass("active");
+      $(this).addClass("active");
+      $(".images").html("");
+      for (let i = 1; i <= 6; i++) {
+        $(".images").append(`<img src='${variant.productImgPath}/${i}.png'/>`);
+      }
+    });
+  });
+});
